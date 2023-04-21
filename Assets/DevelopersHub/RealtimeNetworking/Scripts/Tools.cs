a206aa500b430db1b66b34ab0b0e837ddf3495b5
Armin Ahmadi <75361058+armin-a94@users.noreply.github.com>
2023-04-21 18:37:01 +0330

    Update Tools.cs

--- Assets/DevelopersHub/RealtimeNetworking/Scripts/Tools.cs
+++ Assets/DevelopersHub/RealtimeNetworking/Scripts/Tools.cs
@@ -3,10 +3,12 @@ namespace DevelopersHub.RealtimeNetworking.Client
     using System;
     using System.Collections;
     using System.Collections.Generic;
-    using System.Globalization;
     using System.IO;
+    using System.IO.Compression;
+    using System.Security.Cryptography;
+    using System.Text;
+    using System.Threading.Tasks;
     using System.Xml.Serialization;
-    using UnityEngine;
 
     public static class Tools
     {
@@ -15,6 +17,127 @@ namespace DevelopersHub.RealtimeNetworking.Client
         {
             return Path.GetRandomFileName().Remove(8, 1);
         }
+
+        public static T CloneClass<T>(this T target)
+        {
+            return Desrialize<T>(Serialize<T>(target));
+        }
+
+        public static void CopyTo(Stream source, Stream target)
+        {
+            byte[] bytes = new byte[4096]; int count;
+            while ((count = source.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                target.Write(bytes, 0, count);
+            }
+        }
+
+        #region Encryption
+        public static string EncrypteToMD5(string data)
+        {
+            UTF8Encoding ue = new UTF8Encoding();
+            byte[] bytes = ue.GetBytes(data);
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] hashBytes = md5.ComputeHash(bytes);
+            string hashString = "";
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                hashString = hashString + Convert.ToString(hashBytes[i], 16).PadLeft(2, '0');
+            }
+            return hashString.PadLeft(32, '0');
+        }
+        #endregion
+
+        #region Serialization
+        public static string Serialize<T>(this T target)
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(T));
+            StringWriter writer = new StringWriter();
+            xml.Serialize(writer, target);
+            return writer.ToString();
+        }
+
+        public static T Desrialize<T>(this string target)
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(T));
+            StringReader reader = new StringReader(target);
+            return (T)xml.Deserialize(reader);
+        }
+
+        public async static Task<string> SerializeAsync<T>(this T target)
+        {
+            Task<string> task = Task.Run(() =>
+            {
+                XmlSerializer xml = new XmlSerializer(typeof(T));
+                StringWriter writer = new StringWriter();
+                xml.Serialize(writer, target);
+                return writer.ToString();
+            });
+            return await task;
+        }
+
+        public async static Task<T> DesrializeAsync<T>(this string target)
+        {
+            Task<T> task = Task.Run(() =>
+            {
+                XmlSerializer xml = new XmlSerializer(typeof(T));
+                StringReader reader = new StringReader(target);
+                return (T)xml.Deserialize(reader);
+            });
+            return await task;
+        }
+        #endregion
+
+        #region Compression
+        public async static Task<byte[]> CompressAsync(string target)
+        {
+            Task<byte[]> task = Task.Run(() =>
+            {
+                return Compress(target);
+            });
+            return await task;
+        }
+
+        public static byte[] Compress(string target)
+        {
+            var bytes = Encoding.UTF8.GetBytes(target);
+            using (var msi = new MemoryStream(bytes))
+            {
+                using (var mso = new MemoryStream())
+                {
+                    using (var gs = new GZipStream(mso, CompressionMode.Compress))
+                    {
+                        CopyTo(msi, gs);
+                    }
+                    return mso.ToArray();
+                }
+            }
+        }
+
+        public async static Task<string> DecompressAsync(byte[] bytes)
+        {
+            Task<string> task = Task.Run(() =>
+            {
+                return Decompress(bytes);
+            });
+            return await task;
+        }
+
+        public static string Decompress(byte[] bytes)
+        {
+            using (var msi = new MemoryStream(bytes))
+            {
+                using (var mso = new MemoryStream())
+                {
+                    using (var gs = new GZipStream(msi, CompressionMode.Decompress))
+                    {
+                        CopyTo(gs, mso);
+                    }
+                    return Encoding.UTF8.GetString(mso.ToArray());
+                }
+            }
+        }
+        #endregion
 
     }
 }
